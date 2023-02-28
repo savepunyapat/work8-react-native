8fbc84f0a08eac824642102e9e624539cfdb54eb
savepunyapat <punyapat1412@gmail.com>
2023-02-28 22:56:00 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,108 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,ScrollView, Text, View, FlatList, LogBox, Image } from 'react-native';
+import firebase from 'firebase/compat/app';
+import { getDatabase, ref, onValue, set } from 'firebase/database';
+import { getAuth } from 'firebase/auth';
+import { Provider as PaperProvider, Card, List, Button } from 'react-native-paper';
+import Constants from 'expo-constants';
+import LoginScreen from './screens/Login';
+
+const FirebaseConfig = {
+  apiKey: "AIzaSyD4L87M1OKCAA5_OF1UuxbDWosxKp9tR2o",
+  authDomain: "mobileweblab1.firebaseapp.com",
+  databaseURL: "https://mobileweblab1-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "mobileweblab1",
+  storageBucket: "mobileweblab1.appspot.com",
+  messagingSenderId: "282755090941",
+  appId: "1:282755090941:web:298a12dbef3cd029473194",
+  measurementId: "G-8HQ8CD2VW9"
+};
+
+
+LogBox.ignoreAllLogs(true);
+
+try {
+  firebase.initializeApp(FirebaseConfig);
+} catch (err) { }
+
+function dbListener(path, setData) {
+  const tb = ref(getDatabase(), path);
+  onValue(tb, (snapshot) => {
+    setData(snapshot.val());
+  })
+}
+
+
+function renderCorona({ item }) {
+  var icon = <Image style={{ width: 30, height: 20 }} source={{ uri: `https://covid19.who.int/countryFlags/${item.code}.png` }} />
+  var desc = <View>
+    <Text>{"ผู้ป่วยสะสม " + item.confirmed + " ราย"}</Text>
+    <Text>{"เสียชีวิต " + item.death + " ราย"}</Text>
+    <Text>{"ฉีดวัคซีนต่อประชากร 100 คน : " + item.vaccine + "%"}</Text>
+  </View>;
+  //return <View><Text>ประเทศ {item.name} มีผู้ป่วย {item.confirmed} ราย</Text></View>
+  return <List.Item title={item.name} description={desc} left={(props => icon)}></List.Item>
+}
+
+
+
+function Loading() {
+  return <View><Text>Loading</Text></View>
+}
+
+
 
 export default function App() {
+  const [corona, setCorona] = React.useState([]);
+  const [user, setUser] = React.useState(null);
+
+  React.useEffect(() => {
+    var auth = getAuth();
+    auth.onAuthStateChanged(function (us) {
+      setUser(us);
+    });
+    dbListener("/corona", setCorona);
+    console.log(corona)
+  }, []);
+
+  if (corona.length == 0) {
+    return <Loading />;
+  }
+  if(user==null){    
+    return <LoginScreen/>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Card.Cover source={require("./assets/coronavirus.png")} />
+        <ScrollView>
+          <Card>
+            <Card.Title title="Coronavirus Situation" />
+            <Card.Content>
+              <Text>Your Phone Number: {user.phoneNumber}</Text>
+              <FlatList data={corona} renderItem={renderCorona} ></FlatList>
+            </Card.Content>
+          </Card>
+        </ScrollView>
+        <Button icon="logout" mode="contained" onPress={() => getAuth().signOut()}>
+          Sign Out
+        </Button>
+        <StatusBar backgroundColor="rgba(0,0,0,0.5)" style="light" />
+
+      </View>
+    </PaperProvider>
+
   );
+
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
   },
 });
